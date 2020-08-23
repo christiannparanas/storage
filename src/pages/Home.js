490e8045e5908cbfb7d0afabd45bbd71ce11c655
Christian Paranas <christiannparanas@gmail.com>
2020-08-23 21:25:28 +0800

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 import me from '../assets/img/me.png'
+import web from '../assets/img/computer-mouse.svg'
+import ui from '../assets/img/web-layout.svg'
 
 export default function Home() {
    const [info] = useState("I'm a full stack developer based in Tacloban City, Philippines who loves to build websites and mobile applications.")
@@ -31,6 +33,22 @@ export default function Home() {
          <div className="options">
             <div className="see">Projects</div>
             <div className="cv">Curriculum Vitae <i className='fas fa-file-download'></i></div>
+         </div>
+         <div className="can">
+            <div className="web">
+               <img src={web} alt=""/>
+               <div className="title">Web Development</div>
+               <div className="dis">
+                  I can build your website from simple features to advance, front-end and back-end development.
+               </div>
+            </div>
+            <div className="ui">
+               <img className="ux" src={ui} alt=""/>
+               <div className="title">UX/UI Design</div>
+               <div className="dis">
+                  I can make you simple landing page and complex design for your web app
+               </div>
+            </div>
          </div>
       </motion.div>
    )
