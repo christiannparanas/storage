59509c3525ce269fc7327a1559667815bf20279c
Christian Paranas <christiannparanas@gmail.com>
2020-08-23 18:54:18 +0800

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from 'framer-motion'
+import me from '../assets/img/me.png'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
+   const [info, setInfo] = useState("I'm a full stack developer based in Tacloban City, Philippines who loves to build websites and mobile applications.")
    const containerVariants = {
       hidden: {
          opacity: 0,
@@ -18,7 +21,17 @@ export default function Home() {
 
    return (
       <motion.div className="home" variants={{containerVariants}} initial="hidden" animate="visible" exit="exit">
-         Home
+         <div className="me">
+            <img src={me} alt="dre nadara"/>
+         </div>
+         <div className="msg">
+            <span className="s1">Hi. I'm</span>
+            <span className="s2">Christian R. Paranas</span>
+            <span className="s3">{info}</span>
+         </div>
+         <div className="works">
+            <Link to="/projects" className="link">See my works</Link>
+         </div>
       </motion.div>
    )
 }
