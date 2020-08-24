8e93a6ade73abc7ae92d8714c2e4e46970f9ecba
Christian Paranas <christiannparanas@gmail.com>
2020-08-24 17:45:53 +0800

    projects container

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const Logo = (props) => (
   <Link to="/">
-  <motion.svg whileHover={{ scale: 1.1 }} initial={{ y: -200 }}  animate={{ y: 10 }} transition={{ type: "spring", stiffness: 120}} className="logo" height={200} {...props}>
+  <motion.svg whileHover={{ scale: 1.1 }} initial={{ y: -200 }}  animate={{ y: 10 }} transition={{ type: "spring", stiffness: 120}} className="logo" height={150} {...props}>
     <g>
       <title>{"Layer 1"}</title>
       <ellipse
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,22 +1,42 @@
-import React from 'react'
-import folder from '../assets/img/folder.svg'
-import git from '../assets/img/git.svg'
-import external from '../assets/img/external-link.svg'
+import React from "react";
+import folder from "../assets/img/folder.svg";
+import git from "../assets/img/git.svg";
+import external from "../assets/img/external-link.svg";
 
 export default function Projects() {
+  const works = [
+    {
+      name: "Covid-19 Tracker",
+      disc: "lorem ipsum dolor set amet tae tae gwapo ako sabi nila ng mama ko",
+      tools: ["react", "material ui", "node"],
+    },
+    {
+      name: "Inventory MS",
+      disc: "lorem ipsum dolor set amet tae tae gwapo ako sabi nila ng mama ko",
+      tools: ["react", "material ui"],
+    },
+  ];
 
-   return (
-      <div className="projects">
-         <div className="project-container">
-            <div className="icons">
-               <img src={folder} className="file" alt=""/>
-               <img src={git} className="git" alt=""/>
-               <img src={external} alt=""/>
-            </div>
-            <div className="title">Thea's Food Deliveries</div>
-            <div className="discription">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus quasi consequuntur soluta similique modi excepturi?</div>
-            <div className="tools"></div>
-         </div>
-      </div>
-   )
+  return (
+    <div className="projects">
+      <div className="pro">Projects I built</div>
+
+      {works.map((work) => (
+        <div className="project-container">
+          <div className="icons">
+            <img src={folder} className="file" alt="" />
+            <img src={git} className="git" alt="" />
+            <img src={external} className="exter" alt="" />
+          </div>
+          <div className="title">{work.name}</div>
+          <div className="discription">{work.disc}</div>
+          <div className="tools">
+            {work.tools.map(tool => (
+               <div className="tool">{tool}</div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
